0c68eda81189ed103553498c3f737a5f90c43179
Bulkashmak <mbmhd2015@gmail.com>
2020-09-10 17:05:03 +0200

    Created functionality for adding a 'bookmark it' button on favourite panel in browser

--- images/forms.py
+++ images/forms.py
@@ -1,14 +1,11 @@
 from urllib import request
+from django import forms
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
-from django import forms
 from .models import Image
 
 
 class ImageCreateForm(forms.ModelForm):
-    """
-    ModelForm built from Image model
-    """
     class Meta:
         model = Image
         fields = ('title', 'url', 'description')
@@ -17,19 +14,17 @@ class ImageCreateForm(forms.ModelForm):
         }
 
     def clean_url(self):
-        """
-        Cleans the URL field. Checks whether the file extension is
-        valid.
-        """
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
         extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
-            raise forms.ValidationError('The given URL foes not \
-                match valid image extensions.')
+            raise forms.ValidationError('The given URL does not '
+                                        'match valid image extensions.')
         return url
 
-    def save(self, force_insert=False, force_update=False, commit=True):
+    def save(self, force_insert=False,
+             force_update=False,
+             commit=True):
         image = super().save(commit=False)
         image_url = self.cleaned_data['url']
         name = slugify(image.title)
